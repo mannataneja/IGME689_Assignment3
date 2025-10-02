04c393f3385d623dc7d6212e67e110843f7a5ff9
mannataneja <tt3092@g.rit.edu>
2025-10-02 10:45:12 -0400

    End Screen

--- IGME689_Assignment3/Assets/Scripts/Racing/Checkpoints/DistanceTracker.cs
+++ IGME689_Assignment3/Assets/Scripts/Racing/Checkpoints/DistanceTracker.cs
@@ -9,7 +9,18 @@ public class DistanceTracker : MonoBehaviour
     public int rank;
     public bool isPlayer;
     public TMP_Text rankText;
+    public bool isFinished;
 
+    public Canvas endScreen;
+    private void Start()
+    {
+        if (isPlayer)
+        {
+            isFinished = false;
+            endScreen.enabled = false;
+        }
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "checkpoint")
@@ -17,13 +28,18 @@ public class DistanceTracker : MonoBehaviour
             checkpoint = other.gameObject;
             checkpointDistance = other.gameObject.GetComponent<Checkpoint>().checkpoint;
         }
+        if(other.gameObject.tag == "finished" && distance > 180000)
+        {
+            isFinished = true;
+        }
     }
     private void Update()
     {
         distance = Vector3.Distance(gameObject.transform.position, checkpoint.transform.position) + checkpointDistance;
-        if (isPlayer)
+        if (isPlayer && !isFinished)
         {
             rankText.text = "Rank: " + rank;
+            endScreen.enabled = true;
         }
     }
 }
